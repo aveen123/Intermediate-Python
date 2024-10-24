fde822a7762987603a033444b18dcd4571118a71
Avishek Khatiwada <avishekkhatiwada35@gmail.com>
2024-10-24 19:55:21 -0400

    Learning to create classes and access class variables

---
+++ classes/classes.py
@@ -0,0 +1,7 @@
+# much like a blueprint
+
+class Dog:
+    info = "A furry little creature"
+
+# cannot directly call the variable inside of the class so need to use the ClassName followed by a dot and the variable name
+print(Dog.info)
---
+++ classes/try.py
@@ -0,0 +1,6 @@
+# Create a class for something around you in the room. Create a class variable inside of it
+
+class Laptop:
+    name = "MSI Modern 14 B4MW"
+
+print(Laptop.name)
